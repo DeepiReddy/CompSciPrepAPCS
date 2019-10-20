66fb732991c4cc3d1b051c81ce070ad9ee483b53
DeepiReddy <56797151+DeepiReddy@users.noreply.github.com>
2019-10-20 12:32:45 -0400

    Add files via upload

---
+++ MultipleLetters.java
@@ -0,0 +1,22 @@
+//1. Prints "Enter a name" and gets user input
+//2. Prints each letter in the name, name.length times
+import java.util.Scanner;
+public class MultipleLetters
+{
+
+   public static void main(String[] args)
+   { 
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter a name");
+      String name = in.nextLine();
+      String altName = "";
+      for(int i = 0; i < name.length(); i++)
+      { 
+       for(int n = 0; n < name.length(); n++)
+       {
+        altName += name.charAt(i);
+       }
+      }
+      System.out.println(altName);
+   }
+}
